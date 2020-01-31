15f5e102a297a4990349fc7809457502382fa068
demkantor <demkantor@gmail.com>
2020-01-31 11:15:05 -0600

    and another

--- client.js
+++ client.js
@@ -1,5 +1,47 @@
 $(document).ready(onReady);
 
+// song app
+let song = [];
+
+
 function onReady(){
-    console.log("yo")
+    console.log("high planes drifter");
+    $("#addSongButton").on("click", addSong);
 }
+
+function addSong(){
+    console.log("mellelow like jellelow");
+    let newSong = {
+        artist: $("#artistIn").val(),
+        title: $("#titleIn").val(),
+        album: $("#albumIn").val(),
+        year: $("#yearIn").val(),
+    }
+    song.push(newSong);
+
+    displaySongs();
+
+}
+
+
+
+function displaySongs(){
+    console.log("rolled down my window to watch columbo");
+    let el = $("#songsOut");
+    el.empty();
+    for(let i=0; i<song.length; i++){
+        el.append("<li>" + song[i].artist + "; " + song[i].title + "; " + song[i].album + "; " + song[i].year + "</li>");
+
+    }
+}
+
+
+
+
+///~ test song
+// song.push( {
+//     artist: "Red Hot Chili Pepers",
+//     title: "Under the Bridge",
+//     album: "Blood Surgar Sex Magik",
+//     year: 1991,
+// });
